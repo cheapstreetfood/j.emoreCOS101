08d3581e86ffabcb0e0d04a97d2f014e02c0fdeb
cheapstreetfood <joshuainsert@gmail.com>
2025-10-22 16:18:26 +0100

    Practice 3

---
+++ week-4/practice_3/src/main.rs
@@ -0,0 +1,27 @@
+
+use std::io;
+
+fn main() {
+    let mut input1 = String::new();
+    let mut input2 = String::new();
+
+    println!("\nTriangle Area Calculator");
+
+    println!("Please enter base: ");
+    io::stdin().read_line(&mut input1).expect("Not a valid string");
+    let base:f32 = input1.trim().parse().expect("Not a valid number");
+
+    println!("Please enter height: ");
+    io::stdin().read_line(&mut input2).expect("Not a valid string");
+    let height:f32 = input2.trim().parse().expect("Not a valid number");
+
+    if base > 0.0 {
+        let area:f32 = (base * height) / 2.0 ; 
+        println!("Area of the triangle : {}", area); 
+    }else {
+        println!("Something went wrong D:");
+        
+    }
+
+
+}
