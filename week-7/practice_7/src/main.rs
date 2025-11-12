6f4dcf7910554f2df5b0e62dc6ef1ee539826ec6
cheapstreetfood <joshuainsert@gmail.com>
2025-11-12 16:47:14 +0100

    Using arrays and array length

---
+++ week-7/practice_7/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let arr1:[u8;4] = [10,20,30,40];
+    print!("\nArray with data type");
+    println!("Array is {:?}",arr1);
+    println!("array size is :{}",arr1.len());
+
+    let arr2:[f32;6] = [10.4,20.7,30.4,40.9,51.2,72.2];
+    print!("\nArray with data type");
+    println!("Array is {:?}",arr2);
+    println!("array size is :{}",arr2.len());
+
+    let arr3:[i8;8] = [-1;8];
+    print!("\nArray with data type");
+    println!("Array is {:?}",arr3);
+    println!("array size is :{}",arr3.len());
+}
