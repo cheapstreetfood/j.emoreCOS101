c320ff809edca5a462acb1493391bffeb24f636c
cheapstreetfood <joshuainsert@gmail.com>
2025-11-19 16:37:57 +0100

    Iterating through vector elements with for loops

---
+++ week-8/practice_4/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let o_names = vec!["Ozi","Olivia","Okeke","Oche","Olandia"];
+
+    let o_age = vec![16,18,19,23,28];
+
+    print!("\nAge allocation \n");
+
+    for i in 0..o_age.len()
+    {
+        print!("In O squad , {} is {} years old\n",o_names[i],o_age[i]);
+    }
+
+
+
+}
---
+++ week-8/practice_4/target/debug/practice_4.d
@@ -0,0 +1,1 @@
+C:\Users\joshu\OneDrive\Documents\j.emoreCOS101\week-8\practice_4\target\debug\practice_4.exe: C:\Users\joshu\OneDrive\Documents\j.emoreCOS101\week-8\practice_4\src\main.rs
