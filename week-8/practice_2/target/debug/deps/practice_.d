2e9ffd38601d5102bdd7cdb90d8308f5477f8592
cheapstreetfood <joshuainsert@gmail.com>
2025-11-19 16:21:00 +0100

    Gettin values in a vector using subscripts

---
+++ week-8/practice_2/target/debug/deps/practice_.d
@@ -0,0 +1,5 @@
+C:\Users\joshu\OneDrive\Documents\j.emoreCOS101\week-8\practice_2\target\debug\deps\practice_.d: src\main.rs
+
+C:\Users\joshu\OneDrive\Documents\j.emoreCOS101\week-8\practice_2\target\debug\deps\practice_.exe: src\main.rs
+
+src\main.rs:
---
+++ week-8/practice_2/target/debug/practice_.d
@@ -0,0 +1,1 @@
+C:\Users\joshu\OneDrive\Documents\j.emoreCOS101\week-8\practice_2\target\debug\practice_.exe: C:\Users\joshu\OneDrive\Documents\j.emoreCOS101\week-8\practice_2\src\main.rs
