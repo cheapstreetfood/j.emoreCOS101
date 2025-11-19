96f206969ec6524edfb2d63ba9e546d8c5e2939a
cheapstreetfood <joshuainsert@gmail.com>
2025-11-19 16:16:07 +0100

    Creating vecotrs using method and macro

---
+++ week-8/practice_1/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let v : Vec<u8> = Vec::new();
+
+    println!("\nThe length of Vec::new is {}",v.len());
+
+    let v = vec!["Ozi","Olivia","Okeke","Oche","olandia"];
+    println!("\nThe length of vec macro is : {}",v.len())
+}
