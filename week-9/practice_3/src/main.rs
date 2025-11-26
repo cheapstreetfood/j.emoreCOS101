d3e1039f1e7d121fcb4be34d4b2948096e4e5557
cheapstreetfood <joshuainsert@gmail.com>
2025-11-26 15:33:20 +0100

    Removing files using remove method

---
+++ week-9/practice_3/src/main.rs
@@ -0,0 +1,6 @@
+use std::fs;
+
+fn main() {
+    fs::remove_file("../code_folders/removed_file.txt").expect("could not remove file");
+    println!("file is removed");
+}
---
+++ week-9/practice_3/target/debug/deps/practice_3.d
@@ -0,0 +1,5 @@
+C:\Users\joshu\OneDrive\Documents\j.emoreCOS101\week-9\practice_3\target\debug\deps\practice_3.d: src\main.rs
+
+C:\Users\joshu\OneDrive\Documents\j.emoreCOS101\week-9\practice_3\target\debug\deps\practice_3.exe: src\main.rs
+
+src\main.rs:
