61745923d077cc28d15e2bc78e33e123c848b5b3
cheapstreetfood <joshuainsert@gmail.com>
2025-11-26 15:18:06 +0100

    Creatinf files in rust using methods

---
+++ week-9/practice_1/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\joshu\OneDrive\Documents\j.emoreCOS101\week-9\practice_1\target\debug\deps\practice_1.d: src\main.rs
+
+C:\Users\joshu\OneDrive\Documents\j.emoreCOS101\week-9\practice_1\target\debug\deps\practice_1.exe: src\main.rs
+
+src\main.rs:
